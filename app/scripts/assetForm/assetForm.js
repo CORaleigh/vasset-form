08d1e517609a5512d59973337174916a9d53506a
jkottama201 <julie.kottamala@raleighnc.gov>
2019-12-13 13:40:35 -0500

    prod commit

--- app/scripts/assetForm/assetForm.js
+++ app/scripts/assetForm/assetForm.js
@@ -623,7 +623,7 @@ angular
 
    .factory('assets', ['$http', '$q', function($http, $q){
     var service = {getTables:getTables, getTypes:getTypes, getSites:getSites, checkAssetExists:checkAssetExists, submitAsset:submitAsset},
-      baseUrl = 'https://mapstest.raleighnc.gov/arcgis/rest/services/PublicUtility/Vertical_Assets/FeatureServer';
+      baseUrl = 'https://maps.raleighnc.gov/arcgis/rest/services/PublicUtility/PuVerticalAssets/FeatureServer';
     return service;
     function getTables(token){
       var deferred = $q.defer();
